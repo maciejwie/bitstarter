d4026186bf4d6ab547ac7192cee9d4ecf38714de
Ubuntu <ubuntu@ip-172-31-31-52.us-west-2.compute.internal>
2013-07-11 01:15:07 +0000

    Trying using toString()

--- web.js
+++ web.js
@@ -4,11 +4,9 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  fs.readFile('index.html', 'utf8', function(err,data)) {
-    if (err) {
-      response.send('File not found!');
-    }
-    response.send(data);
+  fs.readFile('index.html', function(err,data)) {
+    if (err) throw err;
+    response.send(data.toString());
   }
 });
 
